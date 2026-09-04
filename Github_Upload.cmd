2984a26285bd2423b7ae2c3b4d6a359ef76e3e38
xhemo <xhemo@github.com>
2026-09-04 18:56:10 +0200

    ORD Tarif Manager Release 04.09.2026 18:56:10

--- Github_Upload.cmd
+++
@@ -1,8 +0,0 @@
-@echo off
-chcp 65001 >nul
-title ORD Tarif Manager - GitHub Upload
-cls
-cd /d "%~dp0"
-powershell.exe -NoProfile -ExecutionPolicy Bypass -Command "& { Unblock-File -Path '%~dp0build\upload_to_github.ps1' -ErrorAction SilentlyContinue; & '%~dp0build\upload_to_github.ps1' }"
-echo.
-pause
